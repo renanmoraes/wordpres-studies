b783b1d5f04ac78cd9347f3cf6520f3027a3ed4d
Renan Moraes <renan.desenvolvimento@gmail.com>
2021-04-01 11:46:19 -0300

    Tecnicas importantes para SEO

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ function wp_config()
     add_theme_support('custom-header',  $args);
     add_theme_support('post-thumbnails');
     add_theme_support('post-formats', array('video', 'image'));
+    add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'wp_config', 0);
--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
-    <title><?php wp_title() ?></title>
     <?php wp_head(); ?>
 </head>
 
